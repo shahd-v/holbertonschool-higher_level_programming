702015fe71d3df16337744dcbf3ea6da0d81b339
shahd faisal <66003305+shahd-v@users.noreply.github.com>
2026-06-06 21:20:15 +0300

    Fix executable permissions

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-
 if __name__ == "__main__":
-    for name in dir(hidden_4):
-        if name[:2] != "__":
-            print(name)
+    import hidden_4
+
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    for name in sorted(names):
+        print(name)
