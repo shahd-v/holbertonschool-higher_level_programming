305ba3f814459efa45abf0e399ad4a4a3f5ff7ad
shahd faisal <66003305+shahd-v@users.noreply.github.com>
2026-06-06 20:49:44 +0300

    task 12 : )

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,8 +3,9 @@ def fizzbuzz():
     for i in range(0, 101):
         if i % 3 == 0:
             print("Fizz", end=" ")
-        if i % 5 == 0:
+        elif i % 5 == 0:
             print("Buzz", end=" ")
-        if i % 3 == 0 and i % 5 == 0:
+        elif i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
-        print("{}".format(i), end=" ")
+        else:
+            print("{}".format(i), end=" ")
