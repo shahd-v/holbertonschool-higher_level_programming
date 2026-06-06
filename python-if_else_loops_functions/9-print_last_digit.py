ab579bdc617c526a5a8caf379f22e43bb4106d6e
shahd faisal <66003305+shahd-v@users.noreply.github.com>
2026-06-06 20:19:49 +0300

    task9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def print_last_digit(number):
+    if number < 0:
+        number = abs(number) % 10
+        number = -number
+        return number
+    else:
+        number = number % 10
+        return number
