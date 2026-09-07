bc519d3833bdf7e2ca42da8998cb49712a873e49
shahd faisal <66003305+shahd-v@users.noreply.github.com>
2026-09-07 14:56:19 +0300

    Get SQLAlchemy state by name

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Displays a State object that matches a supplied name."""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state is None:
+        print("Nothing")
+    else:
+        print(state.id)
+    session.close()
