4a79270e8fbf921917d62cc5b033b79290312c68
shahd faisal <66003305+shahd-v@users.noreply.github.com>
2026-09-09 11:51:56 +0300

    Fetch and display Star Wars movies

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,9 @@
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    data.results.forEach(movie => {
+      const item = document.createElement('li')
+      item.textContent = movie.title
+      document.querySelector('#list_movies').appendChild(item)
+    })
+  })
